a2c5a7d25d1a198a97908064c7bcd4616fe16eba
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-12-04 16:42:59 -0300

    Módulos

--- inicio/app.js
+++ inicio/app.js
@@ -1,10 +1,35 @@
-var mostrarSite = true;
-const site = "www.guiadoprogramador.com";
+const calculadora = require('./calculadora');;
 
-console.log('Hello, world!');
-console.log("Meu nome é Gustavo");
-console.log("E eu estou aprendendo NodeJS com o Guia do Programador");
 
-if (mostrarSite) {
-    console.log(site);
-}
+console.log(calculadora.soma(10, 5));
+console.log(calculadora.sub(10, 5));
+console.log(calculadora.mult(10, 5));
+console.log(calculadora.div(10, 5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var mostrarSite = true;
+// const site = "www.guiadoprogramador.com";
+
+// console.log('Hello, world!');
+// console.log("Meu nome é Gustavo");
+// console.log("E eu estou aprendendo NodeJS com o Guia do Programador");
+
+// if (mostrarSite) {
+//     console.log(site);
+// }
