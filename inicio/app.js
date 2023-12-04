f60b77a0e2a9be98a98ca332f6bfb75f6668d0d7
gustavoalvesdev <gustavoalvesdasilva@outlook.com>
2023-12-04 16:17:54 -0300

    NodeJS na prática

---
+++ inicio/app.js
@@ -0,0 +1,10 @@
+var mostrarSite = true;
+const site = "www.guiadoprogramador.com";
+
+console.log('Hello, world!');
+console.log("Meu nome é Gustavo");
+console.log("E eu estou aprendendo NodeJS com o Guia do Programador");
+
+if (mostrarSite) {
+    console.log(site);
+}
